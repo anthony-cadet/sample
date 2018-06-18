ee00384d340e3cc00dca5cbc843897d46b59eb9c
Anthony <acadet@accengage.com>
2018-06-18 15:37:47 +0200

    [Message] Add button crash on click

--- app/src/main/java/com/accengage/appdemo/messages/MessageActivity.java
+++ app/src/main/java/com/accengage/appdemo/messages/MessageActivity.java
@@ -3,10 +3,17 @@ package com.accengage.appdemo.messages;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.accengage.appdemo.R;
 import com.ad4screen.sdk.Message;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MessageActivity extends AppCompatActivity {
@@ -16,6 +23,10 @@ public class MessageActivity extends AppCompatActivity {
     public static final String MESSAGE_KEY = "message";
 
     private Message message;
+    private List<Message.Button> buttons;
+    private ButtonMessageAdapter mAdapter;
+
+    @BindView(R.id.rv_buttons) RecyclerView rvButtons;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,10 +38,34 @@ public class MessageActivity extends AppCompatActivity {
 
     private void initView() {
         retrieveMessageObject();
+        setupAdapter();
     }
 
     private void retrieveMessageObject() {
+        // Retrieve Message
         Bundle extras = getIntent().getExtras();
         message = extras != null ? (Message) extras.getParcelable(MESSAGE_KEY) : null;
+
+        // Retrieve Buttons
+        int buttonNbr = 0;
+        buttons = new ArrayList<>();
+
+        if (message != null && (buttonNbr = message.countButtons()) <= 0) {
+            Log.d(TAG, "This message haven't buttons");
+            return;
+        }
+
+        for (int i = 0; i < buttonNbr; i++) {
+            buttons.add(message.getButton(i));
+        }
+
+        buttons.get(0).click(getApplicationContext());
+    }
+
+    private void setupAdapter() {
+        mAdapter = new ButtonMessageAdapter(getApplicationContext(), buttons);
+        rvButtons.setAdapter(mAdapter);
+        rvButtons.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
     }
 }
