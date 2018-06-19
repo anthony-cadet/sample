5571a058cc3073b6c2b52ecf5d4562d6df369db0
Anthony <acadet@accengage.com>
2018-06-19 12:33:00 +0200

    [Message] To do : fix crash on click button

--- app/src/main/java/com/accengage/appdemo/messages/MessageActivity.java
+++ app/src/main/java/com/accengage/appdemo/messages/MessageActivity.java
@@ -1,31 +1,47 @@
 package com.accengage.appdemo.messages;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.accengage.appdemo.R;
 import com.ad4screen.sdk.Message;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MessageActivity extends AppCompatActivity {
 
-    private static final String TAG = MessageActivity.class.getName();
-
     public static final String MESSAGE_KEY = "message";
+
+    private static final String TAG = MessageActivity.class.getName();
 
     private Message message;
     private List<Message.Button> buttons;
     private ButtonMessageAdapter mAdapter;
 
+    @BindView(R.id.iv_icon) ImageView ivIcon;
+    @BindView(R.id.tv_title) TextView tvTitle;
+    @BindView(R.id.tv_sender) TextView tvSender;
+    @BindView(R.id.tv_date) TextView tvDate;
+    @BindView(R.id.tv_summary) TextView tvSummary;
+    @BindView(R.id.tv_category) TextView tvCategory;
+    @BindView(R.id.tv_content) TextView tvContent;
+    @BindView(R.id.wv_message) WebView wvMessage;
     @BindView(R.id.rv_buttons) RecyclerView rvButtons;
 
     @Override
@@ -39,6 +55,7 @@ public class MessageActivity extends AppCompatActivity {
     private void initView() {
         retrieveMessageObject();
         setupAdapter();
+        displayContent();
     }
 
     private void retrieveMessageObject() {
@@ -58,8 +75,6 @@ public class MessageActivity extends AppCompatActivity {
         for (int i = 0; i < buttonNbr; i++) {
             buttons.add(message.getButton(i));
         }
-
-        buttons.get(0).click(getApplicationContext());
     }
 
     private void setupAdapter() {
@@ -67,5 +82,52 @@ public class MessageActivity extends AppCompatActivity {
         rvButtons.setAdapter(mAdapter);
         rvButtons.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
+    }
+
+    private void displayContent() {
+        if (message == null) {
+            Log.e(TAG, "Message is null");
+            return;
+        }
+
+        message.getIcon(new Message.onIconDownloadedListener() {
+            @Override
+            public void onIconDownloaded(Bitmap bitmap) {
+                ivIcon.setImageBitmap(bitmap);
+            }
+        });
+        tvTitle.setText(message.getTitle());
+        tvSender.setText(String.format(Locale.FRANCE, "Expéditeur: %s", message.getSender()));
+        tvDate.setText(String.format(Locale.FRANCE, "Reçu le : %s", SimpleDateFormat.getDateTimeInstance().format(message.getSendDate())));
+        tvSummary.setText(message.getText());
+
+        displayCategory();
+        displayMessage();
+    }
+
+    private void displayCategory() {
+        if (message.getCategory() != null && !message.getCategory().isEmpty()) {
+            tvCategory.setText(message.getCategory());
+        } else {
+            tvCategory.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void displayMessage() {
+
+        switch (message.getContentType()) {
+
+            case Web:
+                tvContent.setVisibility(View.GONE);
+                wvMessage.getSettings().setJavaScriptEnabled(true);
+                wvMessage.loadUrl(message.getBody());
+                break;
+
+            case Text:
+                wvMessage.setVisibility(View.GONE);
+                tvContent.setText(message.getBody());
+                break;
+        }
     }
 }
